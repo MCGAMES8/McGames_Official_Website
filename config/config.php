78b0fef7fad8e885f632673d87ce6d3facb868ae
MCGAMES8 <141694902+MCGAMES8@users.noreply.github.com>
2023-09-27 21:21:27 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+
+  // THAT'S THE CURRENT CONFIGURATION
+  // V0.1
+  // MADE BY CATALIN
+
+  $main_color = "#fcb900";
+  $title_of_page = "MC GAMES"; // Change title of the page
+  $description_of_page = "An amazing server!"; // Change the description of your website
+  $ip_server = "2.223.144.35"; // Change the ip
+  $port_server = "25946"; // Change the port
+  $discord_widget_id = "540157437634674708"; // Change discord widget id to get your online members
+  $server_copyright = "Mc Games";
+  $discord_invite_link = "https://discord.gg/UDREHcb88d";
+  $server_logo_width = "400px";
+  $server_logo_height = "auto";
+
+  function returnVotesDescription() {
+    echo('
+    <p>Voting daily for our servers helps us become ranked higher and attract more players!</p>
+    <br>
+    <p>To vote for the server, visit one of the links on the right and enter your Minecraft username.</p>
+    <p>You will automatically be rewarded in-game!</p>
+    <br>
+    <b>Our rewards</b>
+    ');
+  }
+
+
+?>
